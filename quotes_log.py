80b3de7afea19343143a2864a6083dcaf7809277
Joseph Cawthorne <joepersonalv1@gmail.com>
2025-11-19 18:32:51 +0000

    Add quote #14

--- quotes_log.py
+++ quotes_log.py
@@ -23,3 +23,4 @@
 # Done is better than perfect. - Sheryl Sandberg
 # Fall seven times and stand up eight. - Japanese Proverb
 # You miss 100% of the shots you don’t take. - Wayne Gretzky
+# Success is the sum of small efforts, repeated day in and day out. - Robert Collier
