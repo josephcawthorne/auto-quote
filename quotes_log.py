a38e158556bb28cea1337d3c3878d2a87ea2f374
Joseph Cawthorne <joepersonalv1@gmail.com>
2025-12-01 18:37:08 +0000

    Add quote #22

--- quotes_log.py
+++ quotes_log.py
@@ -33,3 +33,4 @@
 # Success is the sum of small efforts, repeated day in and day out. - Robert Collier
 # Motivation gets you going, but discipline keeps you growing. - John C. Maxwell
 # Action is the foundational key to all success. - Pablo Picasso
+# Motivation gets you going, but discipline keeps you growing. - John C. Maxwell
