204b0a3c91829340469090ebf8299aa21e4254a0
Joseph Cawthorne <joepersonalv1@gmail.com>
2025-12-29 18:33:53 +0000

    Add quote #21

--- quotes_log.py
+++ quotes_log.py
@@ -5,7 +5,7 @@
 #######################################################################
 # A simple Python script can automate daily commits - therefore it is not a measure that can be trusted.
 
-# NEXT_COMMIT_DATE: 2025-12-29
+# NEXT_COMMIT_DATE: 2025-12-31
 
 
 # Quote log
@@ -63,3 +63,4 @@
 # You miss 100% of the shots you don’t take. - Wayne Gretzky
 # Whether you think you can or you think you can’t, you’re right. - Henry Ford
 # Motivation gets you going, but discipline keeps you growing. - John C. Maxwell
+# It always seems impossible until it’s done. - Nelson Mandela
