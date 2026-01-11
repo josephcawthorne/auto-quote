9f66fc63ad7399260e8f47d6c95e1c0076ebf9f0
Joseph Cawthorne <joepersonalv1@gmail.com>
2026-01-11 18:32:22 +0000

    Add quote #22

--- quotes_log.py
+++ quotes_log.py
@@ -78,3 +78,4 @@
 # Dream big. Start small. But most of all, start. - Simon Sinek
 # The only way to do great work is to love what you do. - Steve Jobs
 # What we repeatedly do, we become. - Aristotle (paraphrased)
+# Perfection is the enemy of progress. - Winston Churchill
