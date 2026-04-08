45741ff2ded9e97211802b0eba610f6d8dc1d851
Joseph Cawthorne <joepersonalv1@gmail.com>
2026-04-08 19:20:10 +0000

    Add quote #21

--- quotes_log.py
+++ quotes_log.py
@@ -5,7 +5,7 @@
 #######################################################################
 # A simple Python script can automate daily commits - therefore it is not a measure that can be trusted.
 
-# NEXT_COMMIT_DATE: 2026-04-08
+# NEXT_COMMIT_DATE: 2026-04-11
 
 
 # Quote log
@@ -155,4 +155,5 @@
 # The future depends on what you do today. - Mahatma Gandhi
 # The man who moves a mountain begins by carrying away small stones. - Confucius
 # Small deeds done are better than great deeds planned. - Peter Marshall
+# Whether you think you can or you think you can’t, you’re right. - Henry Ford
 # Whether you think you can or you think you can’t, you’re right. - Henry Ford
