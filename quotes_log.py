b3ee69188de5f334d9ce56ae9921a8030a8bbc53
Joseph Cawthorne <joepersonalv1@gmail.com>
2026-04-23 19:14:32 +0000

    Add quote #23

--- quotes_log.py
+++ quotes_log.py
@@ -172,3 +172,4 @@
 # Action is the foundational key to all success. - Pablo Picasso
 # Energy and persistence conquer all things. - Benjamin Franklin
 # Great things are done by a series of small things brought together. - Vincent van Gogh
+# The only way to do great work is to love what you do. - Steve Jobs
