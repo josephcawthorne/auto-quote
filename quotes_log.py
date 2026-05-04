15e9d2728a998bef82390a7dda628dc75413c4aa
Joseph Cawthorne <joepersonalv1@gmail.com>
2026-05-04 19:43:17 +0000

    Add quote #23

--- quotes_log.py
+++ quotes_log.py
@@ -183,3 +183,4 @@
 # Action is the foundational key to all success. - Pablo Picasso
 # The only way to do great work is to love what you do. - Steve Jobs
 # What we repeatedly do, we become. - Aristotle (paraphrased)
+# Great things are done by a series of small things brought together. - Vincent van Gogh
