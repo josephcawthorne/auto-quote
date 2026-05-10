650ebf5ea25ba297820576267939b90b6d369ecf
Joseph Cawthorne <joepersonalv1@gmail.com>
2026-05-10 19:07:48 +0000

    Add quote #22

--- quotes_log.py
+++ quotes_log.py
@@ -188,3 +188,4 @@
 # Success is the sum of small efforts, repeated day in and day out. - Robert Collier
 # What we repeatedly do, we become. - Aristotle (paraphrased)
 # Done is better than perfect. - Sheryl Sandberg
+# Courage is resistance to fear, mastery of fear, not absence of fear. - Mark Twain
