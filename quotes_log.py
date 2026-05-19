9bd0f89165f74bc90173cb50aa7937b35d42f4c1
Joseph Cawthorne <joepersonalv1@gmail.com>
2026-05-19 19:53:42 +0000

    Add quote #22

--- quotes_log.py
+++ quotes_log.py
@@ -196,3 +196,4 @@
 # The only way to do great work is to love what you do. - Steve Jobs
 # Discipline is the bridge between goals and accomplishment. - Jim Rohn
 # Success is the sum of small efforts, repeated day in and day out. - Robert Collier
+# Done is better than perfect. - Sheryl Sandberg
