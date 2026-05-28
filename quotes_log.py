29221139eff0d5911a7de393af03c1295cb1f886
Joseph Cawthorne <joepersonalv1@gmail.com>
2026-05-28 20:26:11 +0000

    Add quote #22

--- quotes_log.py
+++ quotes_log.py
@@ -205,3 +205,4 @@
 # Fall seven times and stand up eight. - Japanese Proverb
 # You miss 100% of the shots you don’t take. - Wayne Gretzky
 # It always seems impossible until it’s done. - Nelson Mandela
+# Courage is resistance to fear, mastery of fear, not absence of fear. - Mark Twain
