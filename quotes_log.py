c9c931f43f92f1fabc241c9586a9d42d5aac69b1
Joseph Cawthorne <joepersonalv1@gmail.com>
2026-06-03 20:57:14 +0000

    Add quote #22

--- quotes_log.py
+++ quotes_log.py
@@ -209,3 +209,4 @@
 # Courage is resistance to fear, mastery of fear, not absence of fear. - Mark Twain
 # Motivation gets you going, but discipline keeps you growing. - John C. Maxwell
 # What we repeatedly do, we become. - Aristotle (paraphrased)
+# Small deeds done are better than great deeds planned. - Peter Marshall
