d8b13f0bbd31a5485dca4e1325d39b5250ff6d44
Joseph Cawthorne <joepersonalv1@gmail.com>
2026-06-08 20:21:06 +0000

    Add quote #22

--- quotes_log.py
+++ quotes_log.py
@@ -215,3 +215,4 @@
 # Great things are done by a series of small things brought together. - Vincent van Gogh
 # What we repeatedly do, we become. - Aristotle (paraphrased)
 # You miss 100% of the shots you don’t take. - Wayne Gretzky
+# Do what you can, with what you have, where you are. - Theodore Roosevelt
