5ac4986985b89a256d9675d2be00ad4ce3f689c6
Joseph Cawthorne <joepersonalv1@gmail.com>
2026-07-03 19:25:25 +0000

    Add quote #21

--- quotes_log.py
+++ quotes_log.py
@@ -5,7 +5,7 @@
 #######################################################################
 # A simple Python script can automate daily commits - therefore it is not a measure that can be trusted.
 
-# NEXT_COMMIT_DATE: 2026-07-03
+# NEXT_COMMIT_DATE: 2026-07-05
 
 
 # Quote log
@@ -237,3 +237,4 @@
 # Courage is resistance to fear, mastery of fear, not absence of fear. - Mark Twain
 # Do what you can, with what you have, where you are. - Theodore Roosevelt
 # The only way to do great work is to love what you do. - Steve Jobs
+# You miss 100% of the shots you don’t take. - Wayne Gretzky
