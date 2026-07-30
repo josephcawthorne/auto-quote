eca0e23126395b2c34b2233cf077f58848418ca4
Joseph Cawthorne <joepersonalv1@gmail.com>
2026-07-30 19:28:03 +0000

    Add quote #23

--- quotes_log.py
+++ quotes_log.py
@@ -265,3 +265,4 @@
 # Done is better than perfect. - Sheryl Sandberg
 # You miss 100% of the shots you don’t take. - Wayne Gretzky
 # It always seems impossible until it’s done. - Nelson Mandela
+# Whether you think you can or you think you can’t, you’re right. - Henry Ford
