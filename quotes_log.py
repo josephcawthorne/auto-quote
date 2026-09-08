151b25dea1cd693ab5e0714e8f0bf91eb21fb354
Joseph Cawthorne <joepersonalv1@gmail.com>
2026-09-08 20:51:09 +0000

    Add quote #23

--- quotes_log.py
+++ quotes_log.py
@@ -307,3 +307,4 @@
 # The future depends on what you do today. - Mahatma Gandhi
 # Small deeds done are better than great deeds planned. - Peter Marshall
 # Whether you think you can or you think you can’t, you’re right. - Henry Ford
+# Action is the foundational key to all success. - Pablo Picasso
